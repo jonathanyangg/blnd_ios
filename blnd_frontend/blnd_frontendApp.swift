3713f572277369872c3a0fc114f4ff41427c094b
jonathanyangg <jonathan.yang.x@gmail.com>
2026-03-03 00:02:36 -0500

    Initial Commit

---
+++ blnd_frontend/blnd_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  blnd_frontendApp.swift
+//  blnd_frontend
+//
+//  Created by Jonathan Yang on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct blnd_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blnd_frontendTests/blnd_frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  blnd_frontendTests.swift
+//  blnd_frontendTests
+//
+//  Created by Jonathan Yang on 03/03/2026.
+//
+
+import Testing
+@testable import blnd_frontend
+
+struct blnd_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
